4e94b7cf0e31c99b3958adb5c2fbce3b00785050
Jelu10 <gela.tsuladze@gmail.com>
2026-04-23 00:55:37 +0200

    Removed unnecessery tracking

--- src/dune_query_id_sets.py
+++ src/dune_query_id_sets.py
@@ -2,47 +2,10 @@ DUNE_QUERY_ID_SETS = {
     "v1": {
         "api_key": "DUNE_API_KEY_v1",
         "query_ids":{
-            "buys": 6308926,
-            "baseline": 6357329,
-            "profit": 6327526,
-            "linked": 6308044
-        }
-    },
-    "v2": {
-        "api_key": "DUNE_API_KEY_v2",
-        "query_ids":
-        {
-            "buys": 6421812,
-            "baseline": 6421849,
-            "profit": 6421862,
-            "linked": 6421879
-        }
-    },
-    "v3": {
-        "api_key": "DUNE_API_KEY_v3",
-        "query_ids":{
-            "buys": 6473962,
-            "baseline": -1,
-            "profit": 6473880,
-            "linked": 6473862
-        }
-    },
-    "v4": {
-        "api_key": "DUNE_API_KEY_v4",
-        "query_ids":{
-            "buys": 6478436,
-            "baseline": -1,
-            "profit": -1,
-            "linked": -1
-        }
-    },
-    "v5": {
-        "api_key": "DUNE_API_KEY_V5",
-        "query_ids":{
-            "buys": 6593408,
-            "baseline":-1,
-            "profit": 6851105,
-            "linked": 6853373
+            "buys": 0,
+            "baseline": 0,
+            "profit": 0,
+            "linked": 0
         }
     }
 }
